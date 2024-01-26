c7812d444ae1512fa153e4bbde563dbd22e23ebe
JongHwan <42468874+whdghks9241@users.noreply.github.com>
2024-01-26 14:11:26 +0900

    로그인 비밀번호 체크코드 수정

--- Server/src/main/java/com/camply/user/controller/UserController.java
+++ Server/src/main/java/com/camply/user/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,6 +31,7 @@ public class UserController {
 	@Autowired
 	private UserService userservice;
 
+	private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
 	@PostMapping("/general/register")
 	public ResponseEntity<String> emailRegister(@RequestBody UserVO userVO) {
@@ -60,7 +63,7 @@ public class UserController {
 
 		 UserVO camplyuservo_info= userservice.getMemberByUsername(userVO.getUSER_EMAIL());
 
-	        if (camplyuservo_info != null && camplyuservo_info.getUSER_PASSWORD().equals(userVO.getUSER_PASSWORD())) {
+	        if (camplyuservo_info != null && passwordEncoder.matches(userVO.getUSER_PASSWORD(), camplyuservo_info.getUSER_PASSWORD())) {
 
 	        	if (camplyuservo_info.getUSER_TYPE().equals("Admin")) {
 		        	user_info.put("USER_ID", camplyuservo_info.getUSER_ID());
