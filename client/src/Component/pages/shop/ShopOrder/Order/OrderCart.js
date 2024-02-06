8958f8b484824b6965be0653cc2947a71c6d9025
rjswh0503 <rjswh0503@yahoo.com>
2024-02-06 10:30:32 +0900

    asd

--- client/src/Component/pages/shop/ShopOrder/Order/OrderCart.js
+++ client/src/Component/pages/shop/ShopOrder/Order/OrderCart.js
@@ -207,7 +207,6 @@ const OrderCart = () => {
                 orderProductPrice: product.productPrice || 0,
                 productThumbnail: product.productThumbnail || '',
                 orderProductName: product.productName || '',
-                 
 
             };
             console.log('orderData:', orderData);
