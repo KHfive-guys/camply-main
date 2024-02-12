9867521fe271e73f27a52f7415933f16cdda27a0
rjswh0503 <rjswh0503@yahoo.com>
2024-02-12 17:29:56 +0900

    Shop

--- client/src/Component/pages/shop/Category/Tent.js
+++ client/src/Component/pages/shop/Category/Tent.js
@@ -7,7 +7,7 @@ import Pagination from "react-js-pagination";
 const Tent = () => {
   const [products, setProducts] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 3;
+  const itemsPerPage = 5;
 
   useEffect(() => {
     const fetchData = async () => {
