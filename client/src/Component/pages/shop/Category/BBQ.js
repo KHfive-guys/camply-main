650e0039c3304fedb2040983300662b400d10710
rjswh0503 <rjswh0503@yahoo.com>
2024-02-12 21:48:00 +0900

    Shop

--- client/src/Component/pages/shop/Category/BBQ.js
+++ client/src/Component/pages/shop/Category/BBQ.js
@@ -1,4 +1,4 @@
-// Tent.js
+
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
@@ -14,27 +14,28 @@ const BBQ = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const startIndex = (currentPage - 1) * itemsPerPage;
-        const endIndex = startIndex + itemsPerPage;
         const response = await axios.get("http://localhost:8080/shop/category/main/fireplace");
-        setProducts(response.data.slice(startIndex, endIndex));
+        setProducts(response.data);
       } catch (error) {
         console.error("상품을 불러오는 중 에러 발생", error);
       }
     };
 
     fetchData();
-  }, [currentPage]);
+  }, []);
+  const startIndex = (currentPage -1 ) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const displayedProducts = products.slice(startIndex, endIndex);
 
   return (
     <>
     <div className='category-item' style={{ display: 'flex', justifyContent: 'center' }}>
-      {products.length > 0 ? (
+      {displayedProducts.length > 0 ? (
         <div>
           <section>
             <h2 style={{ display: 'flex', justifyContent: 'center' }}>화로&BBQ</h2><br />
             <ul className='swiper-wrapper'>
-              {products.map((product) => (
+              {displayedProducts.map((product) => (
                 <li key={product.productId} className='swiper-slide swiper-slide-active' style={{
                   width: "272.5px",
                   marginRight: "30px",
