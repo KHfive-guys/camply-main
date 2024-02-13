233f76197be207ab9b78a8da07f21ef3a4aace3c
JongHwan <whdghks9241@nate.com>
2024-02-13 17:23:52 +0900

    찜하기 기능 추가 완료

--- Server/src/main/java/com/camply/camp/dips/service/DipsService.java
+++ Server/src/main/java/com/camply/camp/dips/service/DipsService.java
@@ -1,11 +1,13 @@
 package com.camply.camp.dips.service;
 
-import com.camply.camp.board.vo.BoardVO;
-import com.camply.camp.dips.dao.DipsDAO;
-import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.camply.camp.dips.dao.DipsDAO;
+import com.camply.camp.dips.vo.DipsVO;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Transactional
@@ -14,11 +16,19 @@ public class DipsService {
 
     @Autowired
     private DipsDAO dipsDAO;
-    public void addCampDips(BoardVO boardVO) {
-        dipsDAO.insertCampDips(boardVO);
+    
+    public void addCampDips(DipsVO dipsvo) {
+
+        dipsDAO.insertCampDips(dipsvo);
     }
 
-    public void removeCampDips(Long camp_id) {
-        dipsDAO.deleteCampDips(camp_id);
+    public void removeCampDips(DipsVO dipsvo) {
+        dipsDAO.deleteCampDips(dipsvo);
+    }
+    
+    // 찜하기 찾기
+    public DipsVO selectCampDips(DipsVO dipsvo) {
+    	
+    	return dipsDAO.selectCampDips(dipsvo);
     }
 }
